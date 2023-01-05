579603c0e6fb0dfbcce322a5b0f4fd00452de30b
bachardaowd <bachar.daowd@gmail.com>
2023-01-05 11:01:19 +0100

    Decrease and increase book quantity

--- src/main/java/com/bachar/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/bachar/springbootlibrary/controller/AdminController.java
@@ -13,17 +13,35 @@ public class AdminController {
     private final AdminService adminService;
 
     @Autowired
-
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
 
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token, AddBookRequest addBookRequest) throws Exception {
-        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
-        if (userEmail == null || !userEmail.equals("admin@email.com")) {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (admin == null || !admin.equals("admin@email.com")) {
             throw new Exception("Administration page only.");
         }
         adminService.postBook(addBookRequest);
     }
+
+    @PostMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token, Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (admin == null || !admin.equals("admin@email.com")) {
+            throw new Exception("Administration page only.");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token, Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (admin == null || !admin.equals("admin@email.com")) {
+            throw new Exception("Administration page only.");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+
 }
